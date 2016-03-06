bebd7a53669b785ba649bed87f203f676916c83d
turquoise <monica.gosschalk@btinternet.com>
2016-03-06 15:56:58 +0000

    Added authorization part two

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :require_signin, except: [:new, :create]
+	before_action :require_correct_user, only: [:edit, :update, :destroy]
 
 	def index
 		@users = User.all
@@ -25,11 +26,11 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		#@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
+		#@user = User.find(params[:id])
 		if @user.update(user_params)
 			redirect_to @user, notice: "Account successfully updated!"
 		else
@@ -38,13 +39,21 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		@user = User.find(params[:id])
+		#@user = User.find(params[:id])
 		@user.destroy
 		session[:user_id] = nil
 		redirect_to root_url, alert: "Account successfully deleted"
 	end
 
 	private
+
+	def require_correct_user
+		@user = User.find(params[:id])
+		unless current_user?(@user)
+			redirect_to root_url
+		end
+	end
+
 	def  user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
